09e12bbf44e581e5a655794ff0217191b19d5966
Diana Mautino <d.mautino305@gmail.com>
2024-10-22 23:10:35 -0500

    Tercera entrega de trabajo final realizada por Diana Mautino

--- tp_transform.py
+++ tp_transform.py
@@ -1,6 +1,6 @@
-import pandas as pd
 from validate_email import validate_email
 import numpy as np
+import pandas as pd
 
 def detectar_duplicados(df, columna, keep='last'):
     """Detecta duplicados en una columna específica de un DataFrame.
@@ -17,14 +17,13 @@ def detectar_duplicados(df, columna, keep='last'):
     return np.where(df[columna].duplicated(keep=keep), 'Duplicado', 'Nuevo')
 
 # Ruta del archivo CSV intermedio
-archivo_csv = 'clientes.csv'
+archivo_csv = 'C:\\ProgramData\\Jenkins\\.jenkins\\jobs\\PIPELINE_TPARCIAL\\clientes.csv'
 
 try:
     data = pd.read_csv(archivo_csv)
     # Leer el archivo CSV    
-    data['Validacion1'] = np.where((data.Telefono.astype(str).str.contains('^[9]')) &
-                                 (data['Telefono'].apply(lambda x : len(str(x))) == 9) &
-                                 (data['Email Cliente'].apply(lambda x:validate_email(x))),'Correcto','Incorrecto')
+    data['Validacion1'] = np.where((data['Telefono'].apply(lambda x : len(str(x))) != 9) &
+                                 (data['Email Cliente'].apply(lambda x: not validate_email(x))),'No Gestionable','Gestionable')
 
     # Combinar fecha y hora
     data['FechaHora'] = pd.to_datetime(data['Fecha Solicitud'] + ' ' + data['Hora Solicitud'], format='%d/%m/%Y %H:%M:%S')
@@ -39,10 +38,20 @@ try:
     data_ordenada = data.sort_values(by='Nombre Completo')
 
     # Exportar a Excel
-    archivo_excel = 'clientes_ordenados.xlsx'
+    archivo_excel = 'C:\\ProgramData\\Jenkins\\.jenkins\\jobs\\PIPELINE_TPARCIAL\\clientes_ordenados.xlsx'
     data_ordenada.to_excel(archivo_excel, index=False)
 
-    print(f"Datos exportados exitosamente a {archivo_excel}")
+    data = pd.read_excel(archivo_excel)
+  # Añadir columna Estado Lead  
+    data['Estado Lead'] = np.where((data['Validacion1'] == 'Gestionable') &
+                                 (data['Estado Ingreso'] =='Nuevo'),'Asignar','No asignar')
+    # Ordenar los datos por nombre
+    data_fasignar = data.sort_values(by='Nombre Completo')
+    # Crear columna Cod Vend
+    data_fasignar['Codigo_vendedor'] = ""
+    # Exportar a Excel
+    archivo2_csv = 'C:\\ProgramData\\Jenkins\\.jenkins\\jobs\\PIPELINE_TPARCIAL\\clientes_EstadoLead.csv'
+    data_fasignar.to_csv(archivo2_csv, index=False)
+    print(f"Datos exportados exitosamente a {archivo_csv}")
 except Exception as e:
     print(f"Error al transformar los datos: {e}")
-    
